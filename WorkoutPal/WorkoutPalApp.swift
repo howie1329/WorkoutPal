8f0f3808bfd8e2e95e1ded1d5d0cd60778ca969e
howie1329 <46204910+howie1329@users.noreply.github.com>
2023-05-05 01:47:25 -0400

    Initial Commit

---
+++ WorkoutPal/WorkoutPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutPalApp.swift
+//  WorkoutPal
+//
+//  Created by Howard Thomas on 5/5/23.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
